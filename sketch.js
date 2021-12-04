7f29d96bfc72f28a8f4258bec0f4bedd6e862350
Weiting Gao <91208552+weitinggao168@users.noreply.github.com>
2021-12-03 23:52:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function resetTree() {
     textSize(40);
     fill(0, 240, 255);
     textFont('Rokkitt');
-    text("Please input your voice", 500, 500);
+    text("Input your voice", 700, 700);
 }
 
 
@@ -97,7 +97,7 @@ function draw() {
         goodFlag = 1;
         textSize(40);
         fill(0, 240, 255);
-        text("Terrific!", 700, 800);
+        text("Terrific!", 750, 850);
     } else {
 
 
